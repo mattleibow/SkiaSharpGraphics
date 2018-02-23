e5bcdd5eea6066664f4ed6788bf1542507140555
Matthew Leibowitz <mattleibow@live.com>
2018-02-24 00:16:17 +0200

    Added the samples

--- SkiaSharpDemo/SkiaSharpDemo/MainPage.xaml.cs
+++ SkiaSharpDemo/SkiaSharpDemo/MainPage.xaml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-using SkiaSharp;
-using SkiaSharp.Views.Forms;
+﻿using Xamarin.Forms;
 
 namespace SkiaSharpDemo
 {
